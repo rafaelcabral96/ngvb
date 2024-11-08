9e8d5ca123d5745c2c30ca5cb2cb95e3e96d5a75
rafaelcabral96 <rafael.medeiroscabral@kaust.edu.sa>
2024-11-08 08:45:37 +0800

    Merge changes from original repository

--- R/ngvb-package.R
+++ R/ngvb-package.R
@@ -5,6 +5,7 @@
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
 #' @importFrom ngme2 rgig
+#' @importFrom GIGrvg rgig
 #' @importFrom scales hue_pal
 #' @importFrom latex2exp TeX
 #' @importFrom ggpubr ggarrange
